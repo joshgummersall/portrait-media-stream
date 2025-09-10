dbcccaa1f6791ba8fd7f31882d0a0a3f9faaa01a
Josh Gummersall <josh@standardlabs.dev>
2025-09-09 21:38:54 -0700

    use non-standard aspect ratio for profit

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 const stream = await navigator.mediaDevices.getUserMedia({
-  video: { aspectRatio: 9 / 16 },
+  video: {
+    aspectRatio: 9 / 16,
+    width: 1280,
+    height: 720,
+    facingMode: "user",
+  },
 });
 
 const video = document.getElementById("stream") as HTMLVideoElement;
